fd784e6ba99a133ca490686b69aa9cd5ed3f2e32
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-05-17 11:56:37 +0700

    update

--- upload_client/src/app/details/details.component.ts
+++ upload_client/src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe((params)=>{
+      let id = params.get('id');
+      //Get product info --> call api
+    })
   }
 
 }
